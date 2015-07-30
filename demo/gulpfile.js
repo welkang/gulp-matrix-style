ee543957b1ee6ba0f333331fd06404fefd269a0f
WELKANG <welkang@qq.com>
2015-07-31 01:21:40 +0800

    add jasmine test

--- demo/gulpfile.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp'),
-    inlineStyle = require('./../index.js');
-
-gulp.task('default', function() {
-    return gulp.src('demo/*.html')
-        .pipe(inlineStyle({}))
-        .pipe(gulp.dest('demo/build/'));
-});
---
+++ spec/main-spec.js
@@ -0,0 +1,6 @@
+describe("test demo", function() {
+    it("b should equal 2", function() {
+        var b = 2;
+        expect(b).toEqual(2);
+    });
+});
